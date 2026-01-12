02330c725bc5749deefaf74518c2187bba076cca
panmalo786 <panmalo786@gmail.com>
2026-01-12 14:49:20 -0800

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    url: process.env["DIRECT_URL"] || process.env["DATABASE_URL"] || "",
   },
   datasource: {
     url: process.env["DATABASE_URL"] || "",
